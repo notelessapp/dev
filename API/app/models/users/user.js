452a659616ba89a57b42dd0b13a4c338bbee71e6
ms9778 <schmidt2@gmail.com>
2016-11-09 12:41:13 +0100

    Added JWT authentication

--- API/app/models/users/user.js
+++ API/app/models/users/user.js
@@ -1,10 +1,18 @@
 // MONGOOSE MODEL FOR A USER COLLECTION
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 var UserSchema = new Schema({
-		username: String,
-		password: String,
+		name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+		password: {
+        type: String,
+        required: true
+    },
 		fullname: String,
 		email: String,
 		paid: Number,
@@ -19,6 +27,38 @@ var UserSchema = new Schema({
 
 }, {versionKey: false});
 
+
+//What to do before actually saving the user
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+
+//How to check the password is correct, a compare password function
+UserSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
 
 
 module.exports = mongoose.model('User', UserSchema);
--- API/server.js
+++ API/server.js
@@ -1,16 +1,37 @@
 // BASIC SETUP
 
 // CALL THE PACKAGES WE NEED
-var express = require('express'); // CALL EXPRESS
-var app = express(); // DEFINING THE APP
-var bodyParser = require ('body-parser'); // CALL BODYPARSER
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var passport  	= require('passport');
+var config      = require('./config/database'); // get db config file
+var User        = require('./app/models/users/user'); // get the mongoose model
+var port        = process.env.PORT || 8080; // SET PORT TO 8080
+var jwt         = require('jwt-simple');
 
 // CONFIGURE APP USING BODYPARSER
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// SET PORT TO 8080
-var port = process.env.PORT || 8080;
+// log to console
+app.use(morgan('dev'));
+
+app.use(function (req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+if (req.method === 'OPTIONS') {
+res.end();
+} else {
+next();
+}
+});
+
+// Use the passport package in our application
+app.use(passport.initialize());
 
 // SETUP ROUTES FOR API
 var router = express.Router();
@@ -20,14 +41,82 @@ router.use(function(req, res, next){
   next();
 });
 
+
+
+// START SERVER
+app.listen(port);
+console.log('Server up and running on ' + port);
+
+// DATABASE CONNECTION
+mongoose.connect(config.database);
+
+// pass passport for configuration
+require('./config/passport')(passport);
 // TEST
-router.get('/', function(req, res){
-  res.json({message:'It works!'});
+app.get('/', function(req, res) {
+  res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+//Userinfo
+
+// route to a restricted info (GET http://localhost:8080/api/memberinfo)
+router.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+    User.findOne({
+      name: decoded.name
+    }, function(err, user) {
+        if (err) throw err;
+
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+          res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+
 // USER ROUTES
-router.route('/users')
 // CREATE A USER
+router.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+
+
+router.route('/users')
+
     .post(function(req, res){
       var user = new User(req.body);
 
@@ -85,6 +174,34 @@ router.route('/users/:user_id')
         res.json({message: 'User deleted!'});
       });
     });
+
+
+//Authentication route
+router.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.encode(user, config.secret);
+          // return the information including token as JSON
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
+
+
 
 // NOTES ROUTES
     router.route('/notes')
@@ -148,16 +265,10 @@ router.route('/users/:user_id')
         });
 
 
-// REGISTERING ROUTES
-app.use('/api', router);
+        // REGISTERING ROUTES
+        app.use('/api', router);
 
-// START SERVER
-app.listen(port);
-console.log('Server up and running on ' + port);
 
-// DATABASE CONNECTION
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://noteless:noteless123@ds139197.mlab.com:39197/notelessdb');
 
 // IMPLEMENTING SCHEMAS
 var Note = require('./app/models/notes/note');
