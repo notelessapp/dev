9a02abc16dac8160b590fb96845a76a930a4df9b
Anders Bergmann <Anders.Bergmann89@gmail.com>
2016-12-07 11:59:07 +0100

    Friends View

--- notelessapp/www/features/friends/friend.controllers.js
+++ notelessapp/www/features/friends/friend.controllers.js
@@ -1,5 +1,13 @@
 angular.module('starter')
 //This is the controller for friends
-.controller('friendCtrl', function($scope, AuthService, $ionicPopup, $state) {
-    
+.controller('friendCtrl', function($scope, AuthService, $ionicPopup, $state, API_ENDPOINT, $http) {
+  //This function is ready if we need to call notes from the db
+  $scope.getFriends = function() {
+    //getting the users notes from the API recievied in an object
+    $http.get(API_ENDPOINT.url + '/memberinfo')
+      .then(function(result) {
+        $scope.friends = result.data.friendslist;
+        console.log($scope.friends);
+    });
+  };
 });
